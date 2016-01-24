7545bad18f145001a258391dfcd44096a3df93f3
DinoLai <dinos80152@gmail.com>
2016-01-25 03:58:33 +0800

    add notes layout

--- posts.js
+++ posts.js
@@ -7,12 +7,13 @@
     });
 
     renderMarkDown();
+    renderNotesList();
 
     function renderMarkDown() {
         var markdownFileName = getUrlLastUri() + '.md';
         getUrlContent('/notes/' + markdownFileName, function(content) {
             document.getElementById('content').innerHTML = marked(content);
-
+            setTitle(document.getElementById('content').getElementsByTagName('h1')[0].innerText);
         });
     }
 
@@ -30,5 +31,25 @@
         };
         xhr.open('GET', url, true);
         xhr.send();
+    }
+
+    function setTitle(titleSuffix) {
+        var title = "DinoLai Personal Page"+ titleSuffix;
+        document.title = title;
+        document.querySelector('meta[property="og:title"]').content = title;
+    }
+
+    function renderNotesList() {
+        var jsonFile = 'notes.json';
+        getUrlContent(jsonFile, function(content) {
+            var noteList = JSON.parse(content);
+            var ul = document.getElementById('notes-list-all');
+            noteList.all.forEach(function(note) {
+                var li = document.createElement('li');
+                li.className = 'list-group-item';
+                li.innerText = note;
+                ul.appendChild(li);
+            });
+        });
     }
 })();
