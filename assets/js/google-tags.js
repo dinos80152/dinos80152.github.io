144e512b2e36b5d8c9ffe08d5ce0aaf1b3ddcd78
DinoLai <dinos80152@gmail.com>
2019-01-17 18:17:27 +0800

    :bug: rewrite google-tags to js version

--- assets/js/google-tags.js
+++ assets/js/google-tags.js
@@ -1,9 +1,16 @@
-// <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-97015161-1"></script>
-<script>
+// Global site tag (gtag.js) - Google Analytics
+(function () {
+  var id = 'UA-97015161-1';
+  var gcse = document.createElement('script');
+  gcse.type = 'text/javascript';
+  gcse.async = true;
+  gcse.src = 'https://www.googletagmanager.com/gtag/js?id=' + id;
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(gcse, s);
+
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'UA-97015161-1');
-</script>
+  gtag('config', id);
+})();
